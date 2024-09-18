891ef617676d5862446611f8f155839e61642a45
Kurious-George <148135288+Kurious-George@users.noreply.github.com>
2024-09-18 16:25:43 -0400

    Delete js directory

--- js/welcome.js
+++
@@ -1,6 +0,0 @@
-const welcomeButton=document.getElementById('welcome-form');
-const heading=document.getElementById('heading');
-welcomeButton.addEventListener('click',function(){
-    const head=prompt("Please enter your name:");
-    heading.textContent='Welcome, '+head+'!';
-});
